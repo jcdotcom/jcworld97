1f5c91c395fd56c8c403052f143323ad0564e7be
user <user@DESKTOP-1OFKN74.home.arpa>
2026-01-19 07:10:31 -0500

    prep for full re-write upload

--- src/Program.tsx
+++
@@ -1,9 +0,0 @@
-export default class Program{
-    title: string;
-    content: string[];
-
-    constructor(title = "null", content = ["o"]){
-        this.title = title;
-        this.content = content;
-    }
-}
